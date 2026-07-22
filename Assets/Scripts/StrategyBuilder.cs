907c29157c4c287c72e6224b8eb60b2de2f9fb12
hillgab <hillgab@OA-S141-27>
2026-07-22 15:12:20 -0400

    Finished the first part of the strategy builder coding gym challenge

--- Assets/Scripts/StrategyBuilder.cs
+++ Assets/Scripts/StrategyBuilder.cs
@@ -1,10 +1,14 @@
 using NUnit.Framework;
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.InputSystem;
 
 public class StrategyBuilder : MonoBehaviour
 {
-    public List<GameObject> Bricks;
+    public GameObject building;
+
+    // private Coroutine brickBuildCoroutine;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,6 +19,26 @@ public class StrategyBuilder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            Instantiate(building);
+        }
+        // brickBuildCoroutine = StartCoroutine(BrickBuild());
     }
+
+    /**private IEnumerator BrickBuild()
+    {
+        float t = 0;
+
+        while (t < brickBuildDuration)
+        {
+            t += Time.deltaTime;
+            transform.localScale = Vector3.one * t / brickBuildDuration;
+
+            yield return null;
+        }
+
+        //WAIT AFTER THE TREE HAS GROWN FOR A SECOND BEFORE GROWING THE APPLES
+        yield return new WaitForSeconds(1);
+    } */
 }
