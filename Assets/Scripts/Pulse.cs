a028e07ae5cf55eae7f11c27c0f3b3931aed872b
hillgab <hillgab@OA-S141-27>
2026-07-29 15:37:23 -0400

    Completed week 12 coding gym

--- Assets/Scripts/Pulse.cs
+++ Assets/Scripts/Pulse.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using UnityEngine;
 
 public class Pulse : MonoBehaviour
@@ -7,26 +8,44 @@ public class Pulse : MonoBehaviour
     private float pulseEndPoint = 9.6f;
     private float pulseStartPoint = -9.6f;
     public AnimationCurve pulseCurve;
+    TrailRenderer trailRenderer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        trailRenderer = GetComponent<TrailRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+        if(pulseProgress > 0)
+        {
+            trailRenderer.emitting = true;
+        }
+
         Vector3 pulseMovement = transform.position;
         pulseMovement.x = Mathf.Lerp(pulseStartPoint, pulseEndPoint, pulseProgress / pulseDuration);
         pulseProgress += Time.deltaTime;
 
-        transform.position = pulseMovement;
         pulseMovement.y = pulseCurve.Evaluate(pulseProgress / pulseDuration);
-        transform.position = pulseMovement;
-        if (pulseProgress > pulseDuration)
+
+        Vector2 heartRateScreenPos = Camera.main.WorldToScreenPoint(pulseMovement);
+
+
+        if (heartRateScreenPos.x >= Screen.width - 7)
         {
+            trailRenderer.emitting = false;
+            
+        }
+        if (heartRateScreenPos.x >= Screen.width){
             pulseProgress = 0;
         }
+
+
+
+            transform.position = pulseMovement;
+
     }
 }
